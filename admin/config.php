9c787a3277c32a020a7b80c6ae515cb326e2ea5a
root <root@localhost>
2016-05-13 15:21:16 +0000

    initial project

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://51975.simplecloud.club/admin/');
+define('HTTP_CATALOG', 'http://51975.simplecloud.club/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://51975.simplecloud.club/admin/');
+define('HTTPS_CATALOG', 'http://51975.simplecloud.club/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/www/htdocs/admin/');
+define('DIR_SYSTEM', '/srv/www/htdocs/system/');
+define('DIR_LANGUAGE', '/srv/www/htdocs/admin/language/');
+define('DIR_TEMPLATE', '/srv/www/htdocs/admin/view/template/');
+define('DIR_CONFIG', '/srv/www/htdocs/system/config/');
+define('DIR_IMAGE', '/srv/www/htdocs/image/');
+define('DIR_CACHE', '/srv/www/htdocs/system/cache/');
+define('DIR_DOWNLOAD', '/srv/www/htdocs/system/download/');
+define('DIR_UPLOAD', '/srv/www/htdocs/system/upload/');
+define('DIR_LOGS', '/srv/www/htdocs/system/logs/');
+define('DIR_MODIFICATION', '/srv/www/htdocs/system/modification/');
+define('DIR_CATALOG', '/srv/www/htdocs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'iatielumegheeph');
+define('DB_PASSWORD', 'seechudiikierai');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://51975.simplecloud.club/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://51975.simplecloud.club/');
+
+// DIR
+define('DIR_APPLICATION', '/srv/www/htdocs/catalog/');
+define('DIR_SYSTEM', '/srv/www/htdocs/system/');
+define('DIR_LANGUAGE', '/srv/www/htdocs/catalog/language/');
+define('DIR_TEMPLATE', '/srv/www/htdocs/catalog/view/theme/');
+define('DIR_CONFIG', '/srv/www/htdocs/system/config/');
+define('DIR_IMAGE', '/srv/www/htdocs/image/');
+define('DIR_CACHE', '/srv/www/htdocs/system/cache/');
+define('DIR_DOWNLOAD', '/srv/www/htdocs/system/download/');
+define('DIR_UPLOAD', '/srv/www/htdocs/system/upload/');
+define('DIR_MODIFICATION', '/srv/www/htdocs/system/modification/');
+define('DIR_LOGS', '/srv/www/htdocs/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'iatielumegheeph');
+define('DB_PASSWORD', 'seechudiikierai');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', 'oc_');
